484182697b77a3ae8ae011feadca7300246eaeee
danielsafari143 <danielsafari143@gmail.com>
2023-07-25 11:39:09 +0200

    ADD CHANGES

--- person.rb
+++ person.rb
@@ -29,9 +29,4 @@ class Person < Nameable
   end
 end
 
-person = Person.new(22, name: 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+
