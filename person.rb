63b4b7b58a4e19b172d914a60e2283a4fc338b0b
danielsafari143 <danielsafari143@gmail.com>
2023-07-25 11:07:56 +0200

    ADD DECORATORS

--- person.rb
+++ person.rb
@@ -1,4 +1,6 @@
-class Person
+require_relative 'nameable'
+
+class Person < Nameable
   def initialize(age, name: 'Unknown', parent_permission: true)
     @id = ''
     @age = age
@@ -11,6 +13,10 @@ class Person
 
   def can_use_services?
     (@age >= 18) or @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
