a37badb339f3d24fbaa458a86d11d646e987f6c9
danielsafari143 <danielsafari143@gmail.com>
2023-07-25 12:49:45 +0200

    ADD CHANGES

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,7 +2,7 @@ require_relative 'decorator'
 
 class TrimmerDecorator < Decorator
   def correct_name
-    return @nameable.correct_name.strip if @nameable.correct_name.length > 10
+    return @nameable.correct_name[0..9] if @nameable.correct_name.length > 10
 
     @nameable.correct_name
   end
