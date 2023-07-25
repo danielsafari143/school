440043287776d4504406f541cf660a5eee2aa6f5
danielsafari143 <danielsafari143@gmail.com>
2023-07-25 11:37:28 +0200

    FIX ERRORS

--- trimmer_decorator.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'decorator'
-
-class TrimmerDecorator < Decorator
-  def correct_name
-    return @nameable.correct_name.strip if @nameable.correct_name.length > 10
-
-    @nameable.correct_name
-  end
-end
