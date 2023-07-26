38267eec7634496182b0994dd60d14aa0a086752
danielsafari143 <danielsafari143@gmail.com>
2023-07-26 02:45:26 +0200

    ADD RENTALS

--- book.rb
+++ book.rb
@@ -1,8 +1,23 @@
+require 'date'
+require_relative 'rental'
+
 class Book
-  attr_accessor :title, :author
+  attr_accessor :title, :author, :rentals
 
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
+  end
+
+  def add_rental(rental)
+    rentals.push(rental)
+    rental.book = (title)
   end
 end
+
+book = Book.new('CodeGame', 'Safari Hamuli')
+rent = Rental.new(Date.today)
+book.add_rental(rent)
+book.add_rental(rent)
+puts book.rentals
